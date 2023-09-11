8a8d70f8b5c47e438cfd6aba8719c5a8fb96f929
Adrianna Nowak <adriannanowakk@gmail.com>
2023-09-11 19:22:20 +0200

    init repo

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,16 @@
+export const convertPLNToUSD = (PLN) => {
+
+  const PLNtoUSD = PLN / 3.5;
+
+  if (typeof (PLN) == 'string') { return NaN }
+  if (PLN === undefined) { return NaN }
+  if (typeof (PLN) !== 'string' && typeof (PLN) !== 'number') { return 'Error' }
+  if (PLN < 0) { return '$0.00' }
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
